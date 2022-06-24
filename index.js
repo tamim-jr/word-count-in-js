334971c8cf77f922525fb1b518c42dbd7ea4a4eb
Tamim Mahmud <internationalinf24@gmail.com>
2022-06-24 20:34:37 +0600

    Word count by js

---
+++ index.js
@@ -0,0 +1,12 @@
+// var speech = "I am tamim and who are you and what are you doung now";
+// console.log(speech.length);
+
+var word = "This is the example of word, here you count all word.";
+var singword = 0;
+for (i = 0; i < word.length; i++) {
+    var char = word[i];
+    if (char == " ") {
+        singword++;
+    }
+}
+console.log(singword);

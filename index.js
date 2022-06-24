71743b1a3f3c74bd8de293f0ef8d2a9ac644c0ff
Tamim Mahmud <internationalinf24@gmail.com>
2022-06-24 20:41:13 +0600

    duble space skip formula

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // var speech = "I am tamim and who are you and what are you doung now";
 // console.log(speech.length);
 
-var word = "This is the example of word, here you count all word.";
+var word = "This is the example of word,  here you  count all word.";
 var singword = 0;
 for (i = 0; i < word.length; i++) {
     var char = word[i];
-    if (char == " ") {
+    if (char == " " && word[i-1] != " ") {
         singword++;
     }
 }
